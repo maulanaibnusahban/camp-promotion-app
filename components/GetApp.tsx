d56f8d3e4ec55b7817c3c07ec4cdfa7aee8478f9
maulanaibnusaban <ibnusaban67@gmail.com>
2024-06-21 21:14:15 +0700

    complate Frature and GetApp

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 function GetApp() {
-  return <div>GetApp</div>;
+  return (
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
+          <p className="regular-16 text-gray-10">Availible on iOS and Android</p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+            <Button type="button" title="Download App" variant="btn_white" icon="/apple.svg" full={true} />
+            <Button type="button" title="Download App" variant="btn_dark_green_outline" icon="/android.svg" full={true} />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" alt="phones" height={550} width={870} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default GetApp;
